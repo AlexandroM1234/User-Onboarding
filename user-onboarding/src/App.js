175ff071be4460cc3ee913cbeca769152fc1a666
Alex Martinez <alexmartinez0071234@gmail.com>
2020-02-27 15:39:26 -0500

    installed dependencies and made form component

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
-
+import Form from "./Components/Form";
 function App() {
   return (
     <div className="App">
-      <p>App</p>
+      <Form />
     </div>
   );
 }
---
+++ user-onboarding/src/Components/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div>
+      <p>This is a test</p>
+    </div>
+  );
+};
+
+export default Form;
