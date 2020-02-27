e6256fdd0a53217fa3542c316c204051b76aeb3c
Alex Martinez <alexmartinez0071234@gmail.com>
2020-02-27 18:46:23 -0500

    it works

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Form, Field, withFormik } from "formik";
 import * as Yup from "yup";
+import axios from "axios";
 
 const NewForm = ({ values, errors, touched, status }) => {
   const [user, setUser] = useState([]);
@@ -20,28 +21,56 @@ const NewForm = ({ values, errors, touched, status }) => {
 
         <label htmlFor="email">Email</label>
         <Field id="email" type="email" name="email" />
+        {touched.email && errors.email && <p>{errors.email}</p>}
 
         <label htmlFor="password">Password</label>
         <Field id="password" type="password" name="password" />
+        {touched.password && errors.password && <p>{errors.password}</p>}
 
         <label htmlFor="termsOfService">Terms of Service</label>
-        <Field type="checkbox" name="checkbox" />
-
+        <Field
+          id="termsOfService"
+          type="checkbox"
+          name="termsOfService"
+          checked={values.termsOfService}
+        />
         <button type="submit">Submit Here</button>
       </Form>
+      {user.map(person => {
+        return (
+          <ul>
+            <li>Name: {person.name}</li>
+            <li>Email: {person.email}</li>
+          </ul>
+        );
+      })}
     </div>
   );
 };
 
 const FormikNewForm = withFormik({
   mapPropsToValues(props) {
-    console.log(props);
     return {
       name: props.name || "",
       email: props.email || "",
       password: props.password || "",
-      TermsofService: props.TermsofService || false
+      TermsofService: props.termsOfService || false
     };
+  },
+
+  validationSchema: Yup.object().shape({
+    email: Yup.string().required(),
+    password: Yup.string().required()
+  }),
+
+  handleSubmit(values, { setStatus, resetForm }) {
+    axios
+      .post("https://reqres.in/api/users", values)
+      .then(Response => {
+        setStatus(Response.data);
+        resetForm();
+      })
+      .catch(error => console.log(error.response));
   }
 })(NewForm);
 
